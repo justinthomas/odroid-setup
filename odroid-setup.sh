4bf8e7c180a90513c0e78c2b093947f25bb1b5d1
loiannog <loiannog@gmail.com>
2013-12-19 21:24:15 +0000

    Adds a setup script

--- odroid-setup.sh
+++ odroid-setup.sh
@@ -1,1 +1,11 @@
+# Source the vimrc file
+echo 'source ~/git/config/vimrc' > ~/.vimrc
+
+# Source the bashrc file
+echo 'source ~/git/config/bashrc' >> ~/.bashrc
+
+# Source the bashrc file from bash_profile
+echo 'source ~/.bashrc' >> ~/.bash_profile
+
+# Add the user to the dialout group for communication on serial
 sudo usermod -a -G dialout odroid
