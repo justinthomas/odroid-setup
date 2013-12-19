34b886f10b0debe8474311d34371cb2301444af1
loiannog <loiannog@gmail.com>
2013-12-19 21:41:31 +0000

    Adds other repositories that are needed

--- odroid-setup.sh
+++ odroid-setup.sh
@@ -7,5 +7,13 @@ echo 'source ~/git/odroid-setup/bashrc' >> ~/.bashrc
 # Source the bashrc file from bash_profile
 echo 'source ~/.bashrc' >> ~/.bash_profile
 
+# Now clone some other repositories
+git clone git@github.com:kartikmohta/quadrotor.git ~/git/quadrotor/
+cd ~/git/quadrotor/
+git submodule init
+git submodule update
+
+git clone git@github.com:loiannog/ibvs_formation_bearing.git ~/git/ibvs_formation_bearing/
+
 # Add the user to the dialout group for communication on serial
 sudo usermod -a -G dialout odroid
