a2c3b923afee33ed5c172b1522d5a3ae3abe9e25
loiannog <loiannog@gmail.com>
2013-12-20 19:02:29 +0000

    updates

--- odroid-setup.sh
+++ odroid-setup.sh
@@ -7,12 +7,16 @@ echo 'source ~/git/odroid-setup/bashrc' >> ~/.bashrc
 # Source the bashrc file from bash_profile
 echo 'source ~/.bashrc' >> ~/.bash_profile
 
-# Now clone some other repositories
+# Now clone the quadrotor repository and set up submodules
 git clone git@github.com:kartikmohta/quadrotor.git ~/git/quadrotor/
 cd ~/git/quadrotor/
 git submodule init
 git submodule update
 
+# clone the odroid_camera repository for the camera drivers
+git clone git@github.com:loiannog/odroid_camera.git ~/git/odroid_camera/
+
+# Formation bearing (image processing code)
 git clone git@github.com:loiannog/ibvs_formation_bearing.git ~/git/ibvs_formation_bearing/
 
 # Add the user to the dialout group for communication on serial
